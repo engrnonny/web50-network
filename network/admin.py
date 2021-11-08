ae2a7b8621ac677ad50276b69e03ae0791937208
engrnonny <engr.nonny@gmail.com>
2021-11-08 11:58:04 +0100

    First commit - Created Post, Like and Comment model

---
+++ network/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import *
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date_added']
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post', 'date_added']
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post', 'date_added']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Like, LikeAdmin)
+admin.site.register(Comment, CommentAdmin)
---
+++ network/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True, null=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True, null=True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True, null=True)
